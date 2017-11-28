b6b73f0bad3aabcf150bdf3c094d369b3b24ec12
boB Rudis <bob@rud.is>
2017-11-28 09:08:27 -0500

    added a delay before redirecting

--- rblogger.js
+++ rblogger.js
@@ -4,10 +4,14 @@ function getElementByXpath(path) {
 
 var x = getElementByXpath(".//aside/following-sibling::p[@class='syndicated-attribution'][1]/following-sibling::div[1]/div[1]/strong/a/@href");
 
+var delay = 5 * 1000; // # seconds to delay the redirect since it's fair to let some lingering
+
 if (x != null) {
   if ("nodeValue" in x) {
     if (x.nodeValue.indexOf("http") !== -1) {
-      document.location = x.nodeValue;  
+      setTimeout(function() {
+        document.location = x.nodeValue; 
+      }, delay);
     }
   }
 }
